5c25c8c44c36636d533b0428c1c5815dd37d1e4b
rkletr <git@merkletr.ee>
2026-04-06 21:54:04 -0400

    feat(caching): put breakpoints on last 3 user messages

--- plugins/caching/index.ts
+++ plugins/caching/index.ts
@@ -11,16 +11,26 @@ export default {
                     ? ({ ...c, cache_control: { type: "ephemeral", ttl: "5m" } })
                     : c)
         });
+
         agent.before("completion", async (e) => {
             const last = e.req.messages.length - 1;
             const u1 = e.req.messages.findLastIndex(m => m.role === "user");
-            const u2 = e.req.messages.slice(0, u1).findLastIndex(m => m.role === "user");
-            if (u1 !== -1)
-                e.req.messages[u1] = setCaching(e.req.messages[u1]);
-            if (u2 !== -1)
-                e.req.messages[u2] = setCaching(e.req.messages[u2]);
+
             if (last !== -1 && last !== u1)
                 e.req.messages[last] = setCaching(e.req.messages[last]);
+
+            if (u1 !== -1) {
+                e.req.messages[u1] = setCaching(e.req.messages[u1]);
+                const u2 = e.req.messages.slice(0, u1).findLastIndex(m => m.role === "user");
+                if (u2 !== -1) {
+                    e.req.messages[u2] = setCaching(e.req.messages[u2]);
+                    const u3 = e.req.messages.slice(0, u2).findLastIndex(m => m.role === "user");
+                    if (u3 !== -1) {
+                        e.req.messages[u3] = setCaching(e.req.messages[u3]);
+                    }
+                }
+            }
+
             return e;
         });
     }
