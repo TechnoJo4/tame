3c97ec0504a05464485733d67457c5cff1edffa4
rkletr <git@merkletr.ee>
2026-05-22 19:16:36 -0400

    rm claudelike for now

--- plugins/claudelike/main.ts
+++
@@ -1,6 +0,0 @@
-import { readTameConfig } from "../../config/index.ts";
-import { configSchema, ClaudelikePlugin } from "./index.ts";
-
-export { configSchema } from "./index.ts";
-
-export default new ClaudelikePlugin(readTameConfig("claudelike.json", configSchema));
