afe2b68c7cdc71bc8801d27684a4d181e698a674
rkletr <git@merkletr.ee>
2026-05-29 12:33:09 -0400

    feat: rm plugin-system-load

--- packages/plugin-system-load/main.ts
+++
@@ -1,5 +0,0 @@
-import { readTameConfig } from "@tame/sdk";
-import { configSchema, SystemLoadPlugin } from "./index.ts";
-
-export { configSchema } from "./index.ts";
-export default new SystemLoadPlugin(readTameConfig("system-load.json", configSchema));
